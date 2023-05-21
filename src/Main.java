f3b581cf5a6a9e435c6f8855359cd7ab42e4e961
hehehelahehe <1063833672@qq.com>
2023-05-21 20:39:35 +0800

    可以播放bgm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import music.MusicThread;
 import view.MainGameFrame;
 
 import javax.swing.*;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +12,26 @@ public class Main {
             MainGameFrame mainFrame = new MainGameFrame(800, 500);
             mainFrame.setVisible(true);
         });
+        File musicFile = new File("resource/Music/09 高级动物.wav");
+
+        URL musicURL = null;
+        try {
+            musicURL = musicFile.toURL();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        // 创建音乐线程实例
+        MusicThread musicThread = new MusicThread(musicURL, true);
+
+        // 创建线程并启动
+        Thread music = new Thread(musicThread);
+        music.start();
+
+        // 调整音量
+        musicThread.setVolume(0.5f); // 设置音量为一半
+        float volume = musicThread.getVolume(); // 获取当前音量
+        System.out.println("volume: " + volume);
     }
 }
---
+++ src/music/MusicThread.java
@@ -0,0 +1,49 @@
+package music;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.net.URL;
+
+public class MusicThread implements Runnable {
+    private URL musicPath;
+    private boolean isLoop;
+    private FloatControl gainControl;
+
+    public MusicThread(URL musicPath, boolean isLoop) {
+        this.musicPath = musicPath;
+        this.isLoop = isLoop;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream ais = AudioSystem.getAudioInputStream(musicPath);
+            clip.open(ais);
+
+            gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+            if (isLoop) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            } else {
+                clip.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setVolume(float volume) {
+        if (gainControl != null) {
+            float dB = (float) (Math.log(volume) / Math.log(10.0) * 20.0);
+            gainControl.setValue(dB);
+        }
+    }
+
+    public float getVolume() {
+        if (gainControl != null) {
+            return (float) Math.pow(10.0, gainControl.getValue() / 20.0);
+        }
+        return 0.0f;
+    }
+}
